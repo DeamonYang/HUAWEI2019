1a73d84e3209d5b04feb540eb8d9b0d2949c77a8
StuLiu <1183862787@qq.com>
2019-03-27 17:40:48 +0800

    init

--- CodeCraft-2019/src/pojo.py
+++ CodeCraft-2019/src/pojo.py
@@ -10,6 +10,7 @@
 @Desciption  : None
 --------------------------------------------------------  
 ''' 
+import queue
 
 class Car(object):
 
@@ -24,8 +25,7 @@ class Car(object):
 		self.car_speed = car_speed
 		self.car_planTime = car_planTime
 
-		self.is_checked = False     # in this clock, false means unchecked, true means checked.
-		self.status = False         # in this clock, false means waiting , ture means terminal.
+		self.is_terminal = False         # false means waiting ,ture means terminal.
 
 	def __str__(self):
 		return str('(' + self.car_id + ','
@@ -44,20 +44,15 @@ class Car(object):
 		return self.car_id
 
 
-
-
-import queue
-
 class Cross(object):
 
 	def __init__(self, cross_id,
-	             cross_road_id_1,
-	             cross_road_id_2,
-	             cross_road_id_3,
-	             cross_road_id_4):
+	             road_id_1,
+	             road_id_2,
+	             road_id_3,
+	             road_id_4):
 		self.cross_id = cross_id
-		self.road_id_list = [cross_road_id_1, cross_road_id_2, cross_road_id_3, cross_road_id_4]
-		self.waiting_list = [queue.Queue(), queue.Queue(), queue.Queue(), queue.Queue()]        # cross buffer
+		self.road_id_list = [road_id_1, road_id_2, road_id_3, road_id_4]
 
 	def __str__(self):
 		return str('(' + self.cross_id + ','
@@ -66,12 +61,19 @@ class Cross(object):
 		           + self.road_id_list[2] + ','
 		           + self.road_id_list[3] + ')')
 
-	def print_waiting_queue(self):
-		for que in self.waiting_list:
-			print(' ********************* ')
-			while que.empty() != True:
-				print(que.get())
-		print('=================================')
+	# def print_waiting_queue(self):
+	# 	for que in self.waiting_queue_dict:
+	# 		print(' ********************* ')
+	# 		while que.empty() != True:
+	# 			print(que.get())
+	# 	print('=================================')
+	#
+	# def push_schedule(self, road_id, schedule_obj):
+	# 	self.waiting_queue_dict[road_id].put(schedule_obj)
+	#
+	# def pop_schedule(self, road_id):
+	# 	schedule = self.waiting_queue_dict[road_id].get()
+	# 	return schedule
 
 
 class Road(object):
@@ -91,16 +93,18 @@ class Road(object):
 		self.road_to = road_to
 		self.road_isDuplex = road_isDuplex
 
-		# self.are_cars_waiting = True
-
 		# 初始化道路的车位列表
-		self.roads_pos = []      # 正方向的车道矩阵，矩阵元素为schedule对象
-		self.roads_neg = []      # 反方向的车道，矩阵元素为schedule对象
+		self.lanes_pos = []      # 正方向的车道矩阵，矩阵元素为schedule对象
+		self.lanes_neg = []      # 反方向的车道，矩阵元素为schedule对象
 		for index in range(self.road_channel):
-			self.roads_pos.append([None] * road_length)
+			self.lanes_pos.append([None] * road_length)
 		if self.road_isDuplex:
 			for index in range(self.road_channel):
-				self.roads_neg.append([None] * road_length)
+				self.lanes_neg.append([None] * road_length)
+
+		# the element of queue is Schedule obj
+		self.waiting_queue_pos = queue.Queue()      # 正向神奇车库发车区
+		self.waiting_queue_neg = queue.Queue()      # 反向神奇车库发车区
 
 	def __str__(self):
 		return str('(' + self.road_id + ','
@@ -113,44 +117,88 @@ class Road(object):
 
 
 	def init_cars_status(self):
-		self.roads_pos = self.__init_cars_status(self.roads_pos)
-		self.roads_neg = self.__init_cars_status(self.roads_neg)
+		"""设置该时刻两个方向车道上车辆的初始状态"""
+		self.lanes_pos = self.__init_cars_status(self.lanes_pos)
+		self.lanes_neg = self.__init_cars_status(self.lanes_neg)
 
-	def __init_cars_status(self, roads):
-		"""设置该时刻车辆的初始状态
+	def __init_cars_status(self, lanes_list):
+		"""设置该时刻指定车道上车辆的初始状态
 		:param roads: [ [schedule, None, schedule, ...], [], [], ...]
 		:return: roads
 		"""
-		for road in roads:
+		for lane in lanes_list:
 			for index in range(self.road_length):
-				if road[index] != None:         # 车道上该位置有车(调度对象)
-					v = min(road[index].car.car_speed, self.road_speed)
-					if index - v < 0:           # 过路口
-						road[index].car.status = False      # 过路口初始状态为等待状态
-					else:   # 不过路口
+				# 车道上该位置有车(调度对象)
+				if lane[index] != None:
+					v = min(lane[index].car.car_speed, self.road_speed)
+					# 过路口
+					if index - v < 0:
+						# 初始状态为等待状态
+						lane[index].car.is_terminal = False
+					# 不过路口
+					else:
+						# 获得前车位置
 						index_of_prev_car = None
 						for j in range(v):
-							if road[index - j - 1] != None:
+							if lane[index - j - 1] != None:
 								index_of_prev_car = index - j - 1
 								break
-						if index_of_prev_car == None:   # 没有车阻挡,当前车前进v路程，并设置为终止状态
-							curr_schedule = road[index]
-							road[index] = None
-							road[index - v] = curr_schedule
-							road[index].car.status = True
-						else:   # 前方有车阻挡
-							if road[index_of_prev_car].car.status == True: # 阻挡车的状态为终止状态
+						# 前方没有车阻挡,当前车前进v路程，并设置为终止状态
+						if index_of_prev_car == None:
+							curr_schedule = lane[index]
+							lane[index] = None
+							lane[index - v] = curr_schedule
+							lane[index].car.is_terminal = True
+						# 前方有车阻挡
+						else:
+							# 阻挡车的状态为终止状态
+							if lane[index_of_prev_car].car.is_terminal == True:
 								# 前进到前方车辆的后面
-								curr_schedule = road[index]
-								road[index] = None
-								road[index_of_prev_car + 1] = curr_schedule
-							else:   # 阻挡车的状态为等待状态
+								curr_schedule = lane[index]
+								lane[index] = None
+								lane[index_of_prev_car + 1] = curr_schedule
+							# 阻挡车的状态为等待状态
+							else:
 								# 原地等待
-								road[index].car.status = False
-					road[index].car.is_checked = True
-				else: # 车道上该位置無车(调度对象)，继续看下一个位置
+								lane[index].car.is_terminal = False
+				# 车道上该位置無车(调度对象)，继续看下一个位置
+				else:
 					continue
-		return roads
+		return lanes_list
+
+	def is_solved(self, cross_id):
+		"""
+		检测每车道第一辆车的状态是否是终止状态
+		:param cross_id: 出路口的id，用来决定方向
+		:return: 全终止则返回True，否则False
+		"""
+		solved_flag = True
+		lanes_list = self.__get_lanes_list_by_cross_id(cross_id)
+		# check first car's status of each road
+		for lane in lanes_list:
+			for car in lane:
+				if car != None:
+					if car.is_terminal == False:
+						solved_flag = False
+					break
+		return solved_flag
+
+	def has_empty_lane(self, cross_id):
+		lanes_list = self.__get_lanes_list_by_cross_id(cross_id)
+		for lane in lanes_list:
+			if lane[-1] == None:
+				return True
+		return False
+
+	def __get_lanes_list_by_cross_id(self, cross_id):
+		if cross_id == self.road_to:
+			return self.lanes_pos
+		elif cross_id == self.road_from:
+			return self.lanes_neg
+		else:
+			raise Exception('Error param corss id:{}.'.format(cross_id))
+
+
 
 class Schedule(object):
 
--- CodeCraft-2019/src/simulator.py
+++ CodeCraft-2019/src/simulator.py
@@ -47,85 +47,82 @@ class Simulator(object):
 
 
 	def run(self):
+		# 如果仍有车辆未到达终点
 		while len(self.__arrived_list) != len(self.__car_list):
 
 			self.__run_cars_in_road()
 
+			# start the cars which are waiting in mysterious park
 			self.__push_cars_to_road_from_queue()
 
-			self.__add_waiting_cars_to_queue()
+			self.__add_cars_to_waiting_queue()
 
 			self.__sys_clock += 1
 
 			print('time:{},arrived count:{}'.format(self.__sys_clock, len(self.__arrived_list)))
-
 		for v in self.__cross_dict.values():
 			v.print_waiting_queue()
 		pass
 
 
 	def __run_cars_in_road(self):
-		# for (/ * 按时间片处理 * /) {
-		# 	while (/ * all car in road run into end state * /){
-		# 		foreach(roads) {
-		# 			/ * 调整所有道路上在道路上的车辆，让道路上车辆前进，只要不出路口且可以到达终止状态的车辆
-		# 			* 分别标记出来等待的车辆（要出路口的车辆，或者因为要出路口的车辆阻挡而不能前进的车辆）
-		# 			* 和终止状态的车辆（在该车道内可以经过这一次调度可以行驶其最大可行驶距离的车辆） * /
-		# 			driveAllCarJustOnRoadToEndState(allChannle); / * 对所有车道进行调整 * /
-		#
-		# 			/ * driveAllCarJustOnRoadToEndState该处理内的算法与性能自行考虑 * /
-		# 		}
-		# 	}
-		#
-		# 	while (/ * all car in road run into end state * /){
-		# 		/ * driveAllWaitCar() * /
-		# 		foreach(crosses){
-		# 			foreach(roads){
-		# 				while (/ * wait car on the road * /){
-		# 					Direction dir = getDirection();
-		# 					Car car = getCarFromRoad(road, dir);
-		# 					if (conflict){
-		# 						break;
-		# 					}
-		#
-		# 					channle = car.getChannel();
-		# 					car.moveToNextRoad();
-		#
-		# 					/ *driveAllCarJustOnRoadToEndState该处理内的算法与性能自行考虑 * /
-		# 				   driveAllCarJustOnRoadToEndState(channel);
-		# 				}
-		# 			}
-		# 		}
-		# 	}
-		# }
 
 		# 遍历道路上车辆由第一排向最后一排进行遍历，确定每辆车的行驶状态
 		for road_id in self.__road_dict:
 			self.__road_dict[road_id].init_cars_status()
 
-		while False:# all cars become the end states
-			pass
-			# run the cars in road
+		# 获得未处理完路口的id，并升序排序
+		unsolved_crosses_id_list = list(self.__cross_dict.keys())
+		unsolved_crosses_id_list.sort()
+		# some cars are still in waiting status in map
+		while len(unsolved_crosses_id_list) > 0:
+			for cross_id in unsolved_crosses_id_list:
+				# current cross to solve
+				curr_cross = self.__cross_dict[cross_id]
+				# move the cars which go to current cross
+				self.__run_cars_by_cross(curr_cross)
+				# check the status of cars which go to current cross
+				solved_flag = True
+				for road_id in curr_cross.road_id_list:
+					if not self.__road_dict[road_id].is_solved(cross_id):
+						solved_flag = False
+						break
+				# if the cars that go to current cross are terminal, the cross solved
+				if solved_flag:
+					unsolved_crosses_id_list.remove(cross_id)
+			# check deadlock begin
+			#
+			#
+			# check deadlock end
 
-			# run cars which through cross
+	def __run_cars_by_cross(self, cross):
+		"""
+		让路口等待的车辆通过路口，并且调整相关道路上车辆的行驶状态
+		:param cross: 当前路口对象
+		"""
+		# run cars which through cross
 
+		# run the cars in road
 
-		# start the cars which are waiting in mysterious park
-		for k in self.__cross_dict.keys():
-			pass
+	def __push_cars_to_road_from_queue(self):
+		"""从路口的神奇车库中启动车辆"""
+		for road_id in self.__road_dict.keys():
+			# if current road has empty lane
+			if True:
+				self.__road_dict[road_id].pop_schedule()
+		pass
 
-
-	def __add_waiting_cars_to_queue(self):
+	def __add_cars_to_waiting_queue(self):
 		"""add the cars to cross waiting areas-mysterious park"""
 		if str(self.__sys_clock + 1) in self.__schedule_dict.keys():
 			# add car to cross buffer
 			for schedule in self.__schedule_dict[str(self.__sys_clock + 1)]:
 				# 起点非终点
 				if len(schedule.road_list) > 0:
-					cross = self.__cross_dict[schedule.car.car_from]
-					road_id_0 = schedule.road_list[0].road_id               # id of the first road
-					road_index = cross.road_id_list.index(road_id_0)
-					self.__cross_dict[schedule.car.car_from].waiting_list[road_index].put(schedule)
+					# id of the first road
+					road_id_0 = schedule.road_list[0].road_id
+					# push schedule object to waiting queue
+					# ...
 				# 起点即终点
 				else:
 					self.__arrived_list.append(schedule)
@@ -133,6 +130,38 @@ class Simulator(object):
 		pass
 
 
-	def __push_cars_to_road_from_queue(self):
+# # 官方路口处理伪代码
+# for (/ * 按时间片处理 * /) {
+# 	while (/ * all car in road run into end state * /){
+# 		foreach(roads) {
+# 			/ * 调整所有道路上在道路上的车辆，让道路上车辆前进，只要不出路口且可以到达终止状态的车辆
+# 			* 分别标记出来等待的车辆（要出路口的车辆，或者因为要出路口的车辆阻挡而不能前进的车辆）
+# 			* 和终止状态的车辆（在该车道内可以经过这一次调度可以行驶其最大可行驶距离的车辆） * /
+# 			driveAllCarJustOnRoadToEndState(allChannle); / * 对所有车道进行调整 * /
+#
+# 			/ * driveAllCarJustOnRoadToEndState该处理内的算法与性能自行考虑 * /
+# 		}
+# 	}
+#
+# 	while (/ * all car in road run into end state * /){
+# 		/ * driveAllWaitCar() * /
+# 		foreach(crosses){
+# 			foreach(roads){
+# 				while (/ * wait car on the road * /){
+# 					Direction dir = getDirection();
+# 					Car car = getCarFromRoad(road, dir);
+# 					if (conflict){
+# 						break;
+# 					}
+#
+# 					channle = car.getChannel();
+# 					car.moveToNextRoad();
+#
+# 					/ *driveAllCarJustOnRoadToEndState该处理内的算法与性能自行考虑 * /
+# 				   driveAllCarJustOnRoadToEndState(channel);
+# 				}
+# 			}
+# 		}
+# 	}
+# }
 
-		pass
